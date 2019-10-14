5f09a74b5dcefefe0b6bc9f4934defba9ed91061
Katherine Sang <sangkatherine@gmail.com>
2019-10-13 21:00:57 -0400

    added 3 nav tabs

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-document.body.className += ' fade-out';
-
-$(function() {
-  $('body').removeClass('fade-out');
+$( "#sidebar__link__personal" ).click(function() {
+  $(".feed__discover").css("display", "none");
+  $(".feed__personal").css("display", "block");
+  $(".feed__newsfeed").css("display", "none");
+  // $("#sidebar__link__personal").css("border-right", "3px solid #F6BD60");
 });
+
+$( "#sidebar__link__discover" ).click(function() {
+  $(".feed__discover").css("display", "block");
+  $(".feed__personal").css("display", "none");
+  $(".feed__newsfeed").css("display", "none");
+  // $("#sidebar__link__discover").css("border-right", "3px solid #F6BD60");
+});
+
+$( "#sidebar__link__newsfeed" ).click(function() {
+  $(".feed__discover").css("display", "none");
+  $(".feed__personal").css("display", "none");
+  $(".feed__newsfeed").css("display", "block");
+  // $("#sidebar__link__newsfeed").css("border-right", "3px solid #F6BD60");
+
+});
+
